26857a643e07b0f5259ddf5783daf6ff51b4daf9
Zavar30 <yarusking@mail.ru>
2022-10-25 22:35:11 +0300

    tests for insertion in freelist

--- tests/freelist/freelisttest.c
+++ tests/freelist/freelisttest.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include "../../zgdb/index/list/freelist.h"
 
 int main() {
@@ -5,7 +6,18 @@ int main() {
     insertNewIndex(pFreeIndexesList, 1);
     insertNewIndex(pFreeIndexesList, 4);
     insertDeadIndex(pFreeIndexesList, 5, 64);
-    insertDeadIndex(pFreeIndexesList, 5, 32);
+
+    struct timespec begin, end;
+
+    for(uint64_t i = 0; i < 20000; i++) {
+        clock_gettime(CLOCK_REALTIME, &begin);
+        insertDeadIndex(pFreeIndexesList, i, 20000 - i);
+        clock_gettime(CLOCK_REALTIME, &end);
+        long nanoseconds = end.tv_nsec - begin.tv_nsec;
+        printf("Ins: %llu, Time measured: %.10ld ns.\n", 20000 - i, nanoseconds);
+        insertDeadIndex(pFreeIndexesList, i, i);
+    }
+
     insertNewIndex(pFreeIndexesList, 9);
     printList(pFreeIndexesList);
     return 0;
